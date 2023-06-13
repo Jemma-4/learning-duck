bf8b70645700d52f0850d172d7ac50f26939dac2
Jemma-4 <1939336810@qq.com>
2023-06-13 16:37:13 +0800

    vuecli生成文件

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <van-button type="warning">警告按钮</van-button>  
+    <div class="test-viewport">测试转换</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+
+
+};
+</script>
+
+<style scoped>
+.test-viewport {
+  width: 200px;
+  height: 100px;
+  font-size: 20px;
+  text-align: center;
+  line-height: 100px;
+  background: #13b5b1;
+}
+</style>
