1129608a79f63320a515b395f990f500f348951f
Jemma-4 <1939336810@qq.com>
2023-06-13 17:00:12 +0800

    组件：帖子列表--信息省略较多用于discovery页面

---
+++ src/views/InvitationList.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="invitaion-list">
+    <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        success-text="刷新成功"
+        @load="onLoad"
+      >
+        <div class="postList">
+          <div
+            v-for="(item,id) in invitationList"
+            :key="id"
+            @click="choseThis(item.workid)"
+            :class="id%2==0?'postItemLeft':'postItem'"
+          >
+            <first-layer-post :detail="item" :userId="userId" id="post" />
+            <div style="height:10px" />
+          </div>
+        </div>
+        <div style="height:5px;" />
+      </van-list>
+    </van-pull-refresh>
+  </div>
+</template>
+
+<script>
+import FirstLayerPost from "../components/FirstLayerPost.vue";
+import { get, post } from "../router/request.js";
+
+export default {
+  name: "InvitationList",
+  components: {
+    FirstLayerPost,
+  },
+  props: {
+    //对接
+    // userId: 0,
+    url: "",
+    type: 0,
+    para: "",
+  },
+  data() {
+    return {
+      invitationList: [],
+      loading: false,
+      finished: false,
+      refreshing: false,
+      invitationPage: 0,
+      maxNum: 0,
+    };
+  },
+  methods: {
+    choseThis(val) {
+      console.log("跳转到帖子详情");
+      this.$router.push("/invitation/" + this.userId + "/" + val);
+    },
+
+    //下拉刷新列表的两个响应函数
+    onLoad() {
+      //对接：列表拉取
+      if (this.refreshing) {
+        this.invitationList = [];
+        this.refreshing = false;
+      }
+      this.invitationPage += 1;
+      get({
+        url: "/api/" + this.url,
+        params: {
+          userid: this.userId,
+          para: this.para,
+          page: this.invitationPage,
+          num: 5, // 每页的条数
+          type: this.type, // 我加入的圈子所有帖子
+        },
+      })
+        .then((res) => {
+          if (res.data.ok == 1) {
+            this.maxNum = res.data.max;
+            // 这里拿数据
+            res.data.workList.forEach((element) => {
+              this.invitationList.push(element);
+            });
+            this.loading = false;
+          }
+          if (this.invitationList.length >= this.maxNum) {
+            this.finished = true;
+          }
+        })
+        .catch((res) => {
+          this.finished = true;
+        });
+    },
+    onRefresh() {
+      // 清空列表数据
+      console.log("refresh");
+      this.finished = false;
+      this.invitationPage = 0;
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      this.loading = true;
+      this.onLoad();
+    },
+  },
+  mounted() {
+    this.userId = this.$cookies.get("userid");
+  },
+};
+</script>
+
+<style scoped>
+#invitaion-list {
+  background-color: rgba(0, 0, 0, 0.05);
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.tip {
+  height: 500px;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.25);
+  text-align: center;
+  line-height: 500px;
+}
+
+.postItemLeft {
+  display: block;
+  width: 172.5px;
+  float: left;
+  clear: left;
+  top: 10px;
+  padding-left: 10px;
+}
+
+.postItem {
+  display: block;
+  width: 172.5px;
+  float: right;
+  padding-right: 10px;
+}
+
+.postList {
+  overflow: hidden;
+  list-style: none;
+}
+</style>
